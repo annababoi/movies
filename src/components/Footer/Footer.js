3b53826baae9e09378ef5fcb7d93c1f1b6bd40b5
annababoi <annababoy07@gmail.com>
2022-10-23 22:51:16 +0300

    done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
             <footer className={css.footer}>
                 <div>
-                    <p>Movie addicts by <span><a href="https://www.linkedin.com/in/anna-baboi-343817235">Anna Baboi</a>.</span></p>
+                    <p>Movie addicts by <span><a target={"_blank"} href="https://www.linkedin.com/in/anna-baboi-343817235">Anna Baboi</a>.</span></p>
                     <p>© 2022 movieAdicts.com</p>
                 </div>
             </footer>
